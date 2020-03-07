055aebb2760f57b8411d66346c2bdfb3cdbf918f
Dustin Martin <36971079+Dusmar6@users.noreply.github.com>
2020-03-06 16:14:21 -0800

    Update UI Intro.py

--- UI Intro.py
+++ UI Intro.py
@@ -4,33 +4,37 @@ import threading
 import GetClip as gc
 import sys
 import os
-
+                              
 import shutil
 import icon
 logo = icon.logo
 
 config = {}
-config['dir'] = ''
+config['dir'] = 'C:\\'
 
 
 def Intro():
     sg.theme('SystemDefaultForReal')
     layout = [  [sg.Text('Welcome to TikBot!', size = (15,1))],
                 [sg.Text('-'*130, text_color = 'light grey')],
-                [sg.Text('Please set your working directory:',  size=(35, 1))],      
+                [sg.Text('Set your working directory:',  size=(35, 1))],      
                      [sg.Text('Your Folder', size=(15, 1), auto_size_text=False, justification='middle'), sg.InputText(config['dir'], key = 'dir'), sg.FolderBrowse()], 
                 [sg.Button('Next'), sg.Button('Cancel')] ]
-    window = sg.Window('TikBot ', layout, icon = logo )
+    window = sg.Window('TikBot ', layout, icon = logo)
+    
+    while True:
         
-    event, values = window.read()
-        
-    if event == 'Next':
-        config['dir'] = os.path.normpath(values['dir'])
-        window.close()
-        
-    if event in (None, 'Cancel'): 
-        window.close()
-        sys.exit()
+        event, values = window.read()
+            
+        if event == 'Next':
+            config['dir'] = os.path.normpath(values['dir'])
+            window.close() 
+            PageTwo()
+            
+        if event in (None, 'Cancel'): 
+            window.close()
+            sys.exit()
+            
     
         
         
@@ -38,12 +42,12 @@ def Intro():
 def PageTwo():
     sg.theme('SystemDefaultForReal')
     layout = [       [sg.Text('Name this collection: ')],
-                     [sg.Text(config['dir'] + '\\', text_color = 'grey'), sg.InputText( key='collection', size= (60,1))],
+                     [sg.Text(os.path.join(config['dir'], 'Tikbot') , text_color = 'grey'), sg.InputText( key='collection', size= (60,1))],
                      [sg.Text('-'*180, text_color = 'light grey')],
                      [sg.Text('Paste the links to each tiktok you would like to add to this collection, seperated by a new line.\nThese can be found by right clicking the video and going to "Inspect Element"')],
                      [sg.Multiline(default_text='', size=(100, 10), key = 'urls')],
                      [sg.Text('-'*180, text_color = 'light grey')],
-                     [sg.Button('Create Collection'), sg.Button('Cancel'), sg.Text(" "*100), sg.Button('Compile Collection', tooltip = 'Edit a collection into a compilation')] ]
+                     [sg.Button('Create Collection'), sg.Button('Back'), sg.Text(" "*110), sg.Button('Compile Collection', tooltip = 'Edit a collection into a single video')] ]
 
     window = sg.Window('TikBot ', layout, icon = logo )
     while True:
@@ -51,17 +55,24 @@ def PageTwo():
         event, values = window.read()
         
         
-        if event in (None, 'Cancel'): 
+        if event == None: 
             break
+        
         if event == "Create Collection":
             
-            collection_path = os.path.join(config['dir'], values['collection'])
+            collection_path = os.path.join(os.path.join(config['dir'], 'Tikbot'), os.path.normpath(values['collection']))
             
             if not DupeCheck(collection_path):
-                os.mkdir(collection_path)
+                os.makedirs(collection_path)
                 thread = threading.Thread(target = gc.GetClip, args = (collection_path, values['urls'].split()))
                 thread.start()
                 
+        if event == "Back":
+            window.close()
+            Intro()
+            
+        if event == "Compile Collection":
+            pop("Not Yet Implemented")
                 
                 
     window.close()
@@ -91,7 +102,7 @@ def DupeCheck(collection):
     
 def main():
     Intro()
-    PageTwo()
+    
     
     
 main()
